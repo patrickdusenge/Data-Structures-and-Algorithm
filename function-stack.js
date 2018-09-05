486df8114b17ce9c07f4f169628aed9b7e04ac11
patrickdusenge <patrickdusenge13@gmail.com>
2018-09-04 20:52:54 -0400

    More testing of stack algorithm

--- function-stack.js
+++ function-stack.js
@@ -40,3 +40,8 @@ myStack.push(2);
 console.log(myStack.peek());
 console.log(myStack.pop());
 console.log(myStack.peek());
+myStack.push("freeCodeCamp");
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
